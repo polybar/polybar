eddda10c2310d82d739cd852059c6235945c6306
Michael Carlberg <c@rlberg.se>
2016-11-30 14:27:37 +0100

    fix(alsa): Replace full alsalib.h

--- include/adapters/alsa.hpp
+++ include/adapters/alsa.hpp
@@ -1,16 +1,38 @@
 #pragma once
 
 #include <assert.h>
-#include <poll.h>
+#include <endian.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <poll.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#ifndef __GNUC__
+#define __inline__ inline
+#endif
+
+#include <alsa/asoundef.h>
+#include <alsa/version.h>
 #include <alsa/global.h>
 #include <alsa/input.h>
 #include <alsa/output.h>
 #include <alsa/error.h>
 #include <alsa/conf.h>
 #include <alsa/pcm.h>
+#include <alsa/rawmidi.h>
+#include <alsa/timer.h>
+#include <alsa/hwdep.h>
 #include <alsa/control.h>
 #include <alsa/mixer.h>
+#include <alsa/seq_event.h>
+#include <alsa/seq.h>
+#include <alsa/seqmid.h>
+#include <alsa/seq_midi_event.h>
 
 #include <cmath>
 #include <functional>
