24a2febd59db242c92f9c6b4655d11c511a3c567
Michael Carlberg <c@rlberg.se>
2016-11-30 21:17:30 +0100

    fix(build): GCC name error

--- include/modules/i3.hpp
+++ include/modules/i3.hpp
@@ -22,13 +22,13 @@ namespace modules {
     };
 
     struct workspace {
-      explicit workspace(int index, state state_, label_t&& label)
+      explicit workspace(int index, enum state state_, label_t&& label)
           : index(index), state(state_), label(forward<label_t>(label)) {}
 
       operator bool();
 
       int index;
-      state state;
+      enum state state;
       label_t label;
     };
 
