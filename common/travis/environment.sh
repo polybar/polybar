1d53e7f4547731e937b392eebf53ac689e06ebf1
Michael Carlberg <c@rlberg.se>
2016-12-15 21:32:05 +0100

    fix(ci): Update libc++ install script

--- common/travis/environment.sh
+++ common/travis/environment.sh
@@ -10,7 +10,10 @@ if [ "${CXX:0:7}" = "clang++" ]; then
   export CXX="clang++-3.8"
   export CC="clang-3.8"
   export CXXFLAGS="${CXXFLAGS} -Qunused-arguments"
-elif [ "${CXX}" = "g++" ]; then
+elif [ "${CXX:0:3}" = "g++" ]; then
+  export CXX="g++-5"
+  export CC="gcc-5"
+elif [ "${CXX:0:3}" = "gcc" ]; then
   export CXX="g++-5"
   export CC="gcc-5"
 fi
--- common/travis/install_libcpp.sh
+++ common/travis/install_libcpp.sh
@@ -7,16 +7,12 @@ if [ "${CXX:0:7}" = "clang++" ]; then
     travis_retry wget --quiet -O - "${LIBCXX_URL}" | tar --strip-components=1 -xJ -C "${LLVM_ROOT}/projects/libcxx"
     travis_retry wget --quiet -O - "${LIBCXXABI_URL}" | tar --strip-components=1 -xJ -C "${LLVM_ROOT}/projects/libcxxabi"
 
-    (cd "${LLVM_ROOT}/build" && cmake .. -DCMAKE_CXX_COMPILER=clang++ && make cxxabi cxx -j2)
+    (cd "${LLVM_ROOT}/build" && cmake .. -DCMAKE_CXX_COMPILER="$CXX" -DCMAKE_C_COMPILER="$CC" -DCMAKE_INSTALL_PREFIX="${LLVM_ROOT}/install" -DCMAKE_BUILD_TYPE=$BUILD_TYPE)
     (cd "${LLVM_ROOT}/build/projects/libcxx" && make install)
     (cd "${LLVM_ROOT}/build/projects/libcxxabi" && make install)
   fi
 
-  export CXXFLAGS="${CXXFLAGS} -I${LLVM_ROOT}/install/include"
-  export CXXFLAGS="${CXXFLAGS} -I${LLVM_ROOT}/install/include/c++/v1"
-  export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
-  export LDFLAGS="${LDFLAGS} -L${LLVM_ROOT}/install/lib"
-  export LDFLAGS="${LDFLAGS} -lc++"
-  export LDFLAGS="${LDFLAGS} -lc++abi"
+  export CXXFLAGS="${CXXFLAGS} -I ${LLVM_ROOT}/install/include/c++/v1"
+  export LDFLAGS="${LDFLAGS} -L ${LLVM_ROOT}/install/lib -lc++ -lc++abi"
   export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${LLVM_ROOT}/install/lib"
 fi
