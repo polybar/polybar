6e53172739d8baeafa6d6a89e7f5505d6b47ac1a
Michael Carlberg <c@rlberg.se>
2016-12-15 19:27:47 +0100

    fix(ci): Add build flags to summary output

--- common/travis/summary.sh
+++ common/travis/summary.sh
@@ -7,3 +7,9 @@ eval "${CC} --version"
 
 echo "cmake --version"
 cmake --version
+
+echo "PATH=${PATH}"
+echo "CXXFLAGS=${CXXFLAGS}"
+echo "LDFLAGS=${LDFLAGS}"
+echo "LD_LIBRARY_PATH=${LD_LIBRARY_PATH}"
+echo "BUILD_TYPE=${BUILD_TYPE}"
