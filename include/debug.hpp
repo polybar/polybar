d94d8ccfd3b5249a78d1793b7208d01488b3b1e2
Michael Carlberg <c@rlberg.se>
2016-12-16 04:01:23 +0100

    feat(debug_util): Scoped execution timer

--- include/debug.hpp
+++ include/debug.hpp
@@ -12,11 +12,26 @@
 POLYBAR_NS
 
 namespace debug_util {
-  template <class T>
-  void loop(const T& expr, size_t iterations) noexcept {
-    while (iterations--) {
-      expr();
+  /**
+   * Wrapper that starts tracking the time when created
+   * and reports the  duration when it goes out of scope
+   */
+  class scope_timer {
+   public:
+    using clock_t = std::chrono::high_resolution_clock;
+    using duration_t = std::chrono::milliseconds;
+
+    explicit scope_timer() : m_start(clock_t::now()) {}
+    ~scope_timer() {
+      std::cout << std::chrono::duration_cast<duration_t>(clock_t::now() - m_start).count() << "ms" << std::endl;
     }
+
+   private:
+    clock_t::time_point m_start;
+  };
+
+  inline unique_ptr<scope_timer> make_scope_timer() {
+    return make_unique<scope_timer>();
   }
 
   template <class T>
@@ -26,11 +41,6 @@ namespace debug_util {
     auto finish = std::chrono::high_resolution_clock::now();
     std::cout << "execution speed: " << std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count()
               << "ms" << std::endl;
-  }
-
-  template <class T>
-  void execution_speed(const T& expr, size_t iterations) noexcept {
-    execution_speed([=] { loop(expr, iterations); });
   }
 
   template <class T>
