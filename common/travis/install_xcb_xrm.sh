ca0d7345a1cde2257610b6c9e8b1fdf6214f7cca
Michael Carlberg <c@rlberg.se>
2017-01-20 04:03:47 +0100

    fix(ci): Update travis

--- common/travis/install_xcb_xrm.sh
+++ common/travis/install_xcb_xrm.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 if [ -z "$(ls -A "${DEPS_DIR}/xcb-util-xrm" 2>/dev/null)" ]; then
   git clone --recursive https://github.com/Airblader/xcb-util-xrm
-  cd xcb-util-xrm || exit 1
-  ./autogen.sh
+  cd xcb-util-xrm
+  ./autogen.sh --prefix=/usr --libdir=/usr/lib
+  make
+  sudo make install
 fi
-
-cd "${DEPS_DIR}/xcb-util-xrm" || exit 1
-make install
