42a982a4a54681ae21c4a870b8a7ed5ed32a3c32
Michael Carlberg <c@rlberg.se>
2017-01-24 13:23:38 +0100

    fix(ci): Cache cmake archive

--- common/travis/install_cmake.sh
+++ common/travis/install_cmake.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ -z "$(ls -A "${DEPS_DIR}/cmake/bin" 2>/dev/null)" ]; then
-  mkdir -p cmake && travis_retry wget --no-check-certificate -O - "${CMAKE_URL}" | tar --strip-components=1 -xz -C cmake
+  mkdir -p "${DEPS_DIR}/cmake" && travis_retry wget --no-check-certificate -O - "${CMAKE_URL}" | tar --strip-components=1 -xz -C "${DEPS_DIR}/cmake"
 fi
 
 export PATH="${DEPS_DIR}/cmake/bin:${PATH}"
