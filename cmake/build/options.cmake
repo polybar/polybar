b97e4c56c5900b9ea216b9da6d05835c2c58ab24
Michael Carlberg <c@rlberg.se>
2017-01-26 12:59:12 +0100

    fix(cmake): Properly check xcb-xrm
    The default value of WITH_XRM was never calculated because
    the "IF NOT DEFINED" check was done after the call to option()
    
    Refs #381

--- cmake/build/options.cmake
+++ cmake/build/options.cmake
@@ -43,6 +43,15 @@ endif()
 
 # }}}
 
+# Default value for: WITH_XRM {{{
+
+if(NOT DEFINED WITH_XRM)
+  pkg_check_modules(XRM QUIET xcb-xrm)
+  set(WITH_XRM ${XRM_FOUND} CACHE BOOL "Enable xcb-xrm support")
+endif()
+
+# }}}
+
 # Define build options {{{
 
 option(CXXLIB_CLANG       "Link against libc++"        OFF)
@@ -73,11 +82,6 @@ option(WITH_XSYNC         "xcb-sync support"           OFF)
 option(WITH_XCOMPOSITE    "xcb-composite support"      OFF)
 option(WITH_XKB           "xcb-xkb support"            ON)
 option(WITH_XRM           "xcb-xrm support"            ON)
-
-if(NOT DEFINED WITH_XRM)
-  pkg_check_modules(XRM QUIET xcb-xrm)
-  set(WITH_XRM ${XRM_FOUND} CACHE BOOL "Enable xcb-xrm support")
-endif()
 
 if(NOT DEFINED ENABLE_XRANDR_MONITORS)
   pkg_check_modules(XRANDR QUIET xrandr>=1.5.0 randrproto>=1.5.0 xcb-proto>=1.12)
