f9a2e1a186bf066136435ee28f9c3e97f105faca
patrick96 <p.ziegler96@gmail.com>
2018-01-06 18:45:37 +0100

    feat(bspwm): Add workspace separator
    Same as the i3 workspace separator

--- include/modules/bspwm.hpp
+++ include/modules/bspwm.hpp
@@ -75,6 +75,11 @@ namespace modules {
     label_t m_monitorlabel;
     iconset_t m_icons;
 
+    /**
+     * Separator that is inserted in between workspaces
+     */
+    label_t m_labelseparator;
+
     bool m_click{true};
     bool m_scroll{true};
     bool m_revscroll{true};
