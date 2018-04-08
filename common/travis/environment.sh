495920db9c3d44510df96ac033a0a25fbbe56f19
patrick96 <p.ziegler96@gmail.com>
2018-04-08 14:32:02 +0200

    travis: Don't install specific compilers
    clang is already installed in travis' trusty environment
    
    Should decrease build time quite a bit because we don't need to download
    and compule libc++

--- common/travis/environment.sh
+++ common/travis/environment.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 # Update compiler flags
-if [ "${CXX:0:7}" = "clang++" ]; then
-  export CXX="clang++-3.8"
-  export CC="clang-3.8"
+if [ "${CXX}" = "clang++" ]; then
   export CXXFLAGS="${CXXFLAGS} -Qunused-arguments"
-elif [ "${CXX:0:3}" = "g++" ]; then
+elif [ "${CXX}" = "g++" ]; then
   export CXX="g++-5"
   export CC="gcc-5"
 fi
