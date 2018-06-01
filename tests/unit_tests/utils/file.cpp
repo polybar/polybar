78680a6832dafa1cbc3ba6b7fe36144c0d53c9da
patrick96 <p.ziegler96@gmail.com>
2018-06-01 15:25:11 +0200

    tests: Convert file test to gtest

--- tests/unit_tests/utils/file.cpp
+++ tests/unit_tests/utils/file.cpp
@@ -5,12 +5,13 @@
 #include "utils/command.hpp"
 #include "utils/file.hpp"
 
-int main() {
-  using namespace polybar;
+using namespace polybar;
 
-  "expand"_test = [] {
-    auto cmd = command_util::make_command("echo $HOME");
-    cmd->exec();
-    cmd->tail([](string home) { expect(file_util::expand("~/test") == home + "/test"); });
-  };
+TEST(File, expand) {
+  auto cmd = command_util::make_command("echo $HOME");
+  cmd->exec();
+  cmd->tail([](string home) {
+      EXPECT_EQ(home + "/test", file_util::expand("~/test"));
+      });
 }
+
