0885cdf02d670dfd8e02c0a51b787abde17324eb
Ryan Geary <rtgnj42@gmail.com>
2018-10-15 17:33:43 -0400

    Add ramp-padding capabilities to module/cpu

--- src/modules/cpu.cpp
+++ src/modules/cpu.cpp
@@ -18,6 +18,8 @@ namespace modules {
   cpu_module::cpu_module(const bar_settings& bar, string name_) : timer_module<cpu_module>(bar, move(name_)) {
     m_interval = m_conf.get<decltype(m_interval)>(name(), "interval", 1s);
 
+    m_ramp_padding = m_conf.get<decltype(m_ramp_padding)>(name(), "ramp-padding");
+
     m_formatter->add(DEFAULT_FORMAT, TAG_LABEL, {TAG_LABEL, TAG_BAR_LOAD, TAG_RAMP_LOAD, TAG_RAMP_LOAD_PER_CORE});
 
     // warmup cpu times
@@ -97,7 +99,7 @@ namespace modules {
       auto i = 0;
       for (auto&& load : m_load) {
         if (i++ > 0) {
-          builder->space(1);
+          builder->space(m_ramp_padding);
         }
         builder->node(m_rampload_core->get_by_percentage(load));
       }
