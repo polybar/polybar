95e896c3454ddb6408c988649ca0cdd54be63d85
Sam Lunt <samuel.j.lunt@gmail.com>
2018-10-14 22:53:27 -0500

    Change file_util::expand, add file_util::is_absolute and file_util::dirname
    file_util::expand now:
    - strips repeated file separators
    - removes '.' and '..' from file names
    - home directory expansion works for other users
    - home directories are only expanded at the start of filenames
    
    file_util::is_absolute returns if a path name is an absolute path
    file_util::dirname strips the last component from a filename

--- tests/unit_tests/utils/file.cpp
+++ tests/unit_tests/utils/file.cpp
@@ -7,11 +7,93 @@
 
 using namespace polybar;
 
-TEST(File, expand) {
-  auto cmd = command_util::make_command("echo $HOME");
-  cmd->exec();
-  cmd->tail([](string home) {
-      EXPECT_EQ(home + "/test", file_util::expand("~/test"));
-      });
+namespace {
+
+  auto expand_with_shell(string const& path) -> string
+  {
+    auto cmd = command_util::make_command("echo " + path);
+    cmd->exec();
+
+    string result;
+    cmd->tail([&result](string output) {
+      result = std::move(output);
+    });
+
+    return result;
+  }
+
 }
 
+TEST(File, expand) {
+  // '/' is inserted to relative paths
+  EXPECT_EQ(file_util::expand("a"), "/a");
+
+  // '.' and '..' are handled
+  EXPECT_EQ(file_util::expand("/a/./b"), "/a/b");
+  EXPECT_EQ(file_util::expand("/a/b/../c"), "/a/c");
+  EXPECT_EQ(file_util::expand("/a/b/./././c"), "/a/b/c");
+
+  // consecutive '/'s are stripped
+  EXPECT_EQ(file_util::expand("/a/b////c"), "/a/b/c");
+
+  // trailing '/' is preserved
+  EXPECT_EQ(file_util::expand("a/"), "/a/");
+  EXPECT_EQ(file_util::expand("/a/./b/"), "/a/b/");
+
+  EXPECT_EQ(file_util::expand(""), "/");
+  EXPECT_EQ(file_util::expand("/"), "/");
+  EXPECT_EQ(file_util::expand("//"), "/");
+  EXPECT_EQ(file_util::expand("/../"), "/");
+  EXPECT_EQ(file_util::expand("/../.."), "/");
+  EXPECT_EQ(file_util::expand("/..////.."), "/");
+  EXPECT_EQ(file_util::expand("/.././.././.."), "/");
+  EXPECT_EQ(file_util::expand("."), "/");
+
+  // expand home directories
+  EXPECT_EQ(file_util::expand("~/test"), expand_with_shell("~/test"));
+  EXPECT_EQ(file_util::expand("./$HOME/test"), expand_with_shell("$HOME/test"));
+
+  EXPECT_NE(file_util::expand("~root/test"), "~root/test");
+  EXPECT_EQ(file_util::expand("~root/test"), expand_with_shell("~root/test"));
+
+  auto user_home = "~" + expand_with_shell("$USER") + "/test";
+  EXPECT_NE(file_util::expand(user_home), user_home);
+  EXPECT_EQ(file_util::expand(user_home), expand_with_shell(user_home));
+
+  // only leading tilde is expanded
+  EXPECT_EQ(file_util::expand("/tmp/~"), "/tmp/~");
+  EXPECT_EQ(file_util::expand("/tmp/~root"), "/tmp/~root");
+  EXPECT_EQ(file_util::expand("~/~"), expand_with_shell("~/~"));
+  EXPECT_EQ(file_util::expand("~/~"), expand_with_shell("$HOME/~"));
+
+  // HOME is expanded anywhere
+  EXPECT_EQ(file_util::expand("/tmp/$HOME"), expand_with_shell("/tmp$HOME"));
+  EXPECT_EQ(file_util::expand("/tmp/$HOME"), "/tmp" + expand_with_shell("~"));
+}
+
+TEST(File, dirname) {
+  // from dirname(3) manpage
+  EXPECT_EQ(file_util::dirname("/usr/lib"), "/usr");
+  EXPECT_EQ(file_util::dirname("/usr/"), "/");
+  EXPECT_EQ(file_util::dirname("usr"), ".");
+  EXPECT_EQ(file_util::dirname("/"), "/");
+  EXPECT_EQ(file_util::dirname("."), ".");
+  EXPECT_EQ(file_util::dirname(".."), ".");
+  EXPECT_EQ(file_util::dirname(""), ".");
+}
+
+TEST(File, isAbsolute) {
+  EXPECT_TRUE(file_util::is_absolute("/usr/lib"));
+  EXPECT_TRUE(file_util::is_absolute("/usr/"));
+  EXPECT_TRUE(file_util::is_absolute("/"));
+
+  EXPECT_TRUE(file_util::is_absolute("~"));
+  EXPECT_TRUE(file_util::is_absolute("~/"));
+  EXPECT_TRUE(file_util::is_absolute("~root"));
+  EXPECT_TRUE(file_util::is_absolute("~root/"));
+
+  EXPECT_FALSE(file_util::is_absolute("usr"));
+  EXPECT_FALSE(file_util::is_absolute("."));
+  EXPECT_FALSE(file_util::is_absolute(".."));
+  EXPECT_FALSE(file_util::is_absolute(""));
+}
