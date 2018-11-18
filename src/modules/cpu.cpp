8c7b4e400659bf7550c473f0247afc146d6b8132
BigRedEye <skvor.sm@gmail.com>
2018-11-18 11:49:21 +0300

    feat(cpu): Add normalize-load

--- src/modules/cpu.cpp
+++ src/modules/cpu.cpp
@@ -20,6 +20,8 @@ namespace modules {
 
     m_ramp_padding = m_conf.get<decltype(m_ramp_padding)>(name(), "ramp-coreload-spacing", 1);
 
+    m_normalize_load = m_conf.get<bool>(name(), "normalize-load", true);
+
     m_formatter->add(DEFAULT_FORMAT, TAG_LABEL, {TAG_LABEL, TAG_BAR_LOAD, TAG_RAMP_LOAD, TAG_RAMP_LOAD_PER_CORE});
 
     // warmup cpu times
@@ -64,7 +66,9 @@ namespace modules {
       }
     }
 
-    m_total = m_total / static_cast<float>(cores_n);
+    if (m_normalize_load) {
+      m_total /= static_cast<float>(cores_n);
+    }
 
     if (m_label) {
       m_label->reset_tokens();
