b4ae455a129c2a43a2d4f35cba12f022d1126b4c
Edouard Veyrier <edouard.veyrier@epita.fr>
2018-12-06 11:59:53 +0100

    feat(script): add format-else (#346)

--- include/modules/script.hpp
+++ include/modules/script.hpp
@@ -7,6 +7,8 @@
 POLYBAR_NS
 
 namespace modules {
+  enum script_state { NONE = 0, IF, ELSE };
+
   class script_module : public module<script_module> {
    public:
     explicit script_module(const bar_settings&, string);
@@ -15,14 +17,18 @@ namespace modules {
     void start();
     void stop();
 
+    string get_format() const;
     string get_output();
     bool build(builder* builder, const string& tag) const;
 
    protected:
     chrono::duration<double> process(const mutex_wrapper<function<chrono::duration<double>()>>& handler) const;
-    bool check_condition();
+    int check_condition();
+    bool has_else_format() const;
+    string get_exec();
 
    private:
+    static constexpr auto FORMAT_ELSE = "format-else";
     static constexpr const char* TAG_LABEL{"<label>"};
 
     mutex_wrapper<function<chrono::duration<double>()>> m_handler;
@@ -34,7 +40,6 @@ namespace modules {
     string m_exec;
     string m_exec_if;
     string m_exec_else;
-    string* m_exec_ptr;
 
     chrono::duration<double> m_interval{0};
     map<mousebtn, string> m_actions;
@@ -43,6 +48,7 @@ namespace modules {
     string m_output;
     string m_prev;
     int m_counter{0};
+    int m_if_state;
 
     bool m_stopping{false};
   };
