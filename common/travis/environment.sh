ea5fc22c7d43d7f4d25855124b275159f43c3371
patrick96 <p.ziegler96@gmail.com>
2018-12-23 23:14:55 +0100

    travis: Upgrade to xenial
    This travis environment contains more up-to-date versions of clang and
    gcc (namely 7.0.0 and 5.4.0). This allows us to remove all the code used
    to get those newer versions in trusty.
    
    Ref: https://docs.travis-ci.com/user/reference/xenial/

--- common/travis/environment.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Update compiler flags
-if [ "${CXX}" = "clang++" ]; then
-  export CXXFLAGS="${CXXFLAGS} -Qunused-arguments"
-elif [ "${CXX}" = "g++" ]; then
-  export CXX="g++-5"
-  export CC="gcc-5"
-fi
