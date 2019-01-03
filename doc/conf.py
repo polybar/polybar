bbdcbee068fb324199e9522fd4187e3db1481dcb
patrick96 <p.ziegler96@gmail.com>
2019-01-03 20:24:21 +0100

    doc: Make conf.py work on readthedocs
    rtd scans the project for a conf.py file so we cannot name it conf.py.in
    unless we get rtd to run cmake before building.
    
    The easier option is to have doc/conf.py be the file used by rtd and all
    other builds use cmake to first configure it.

--- doc/conf.py
+++ doc/conf.py
@@ -28,6 +28,17 @@ version = '@APP_VERSION@'
 # The full version, including alpha/beta/rc tags
 release = version
 
+# is whether we are on readthedocs.io
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+# Set path to documentation
+if on_rtd:
+  # On readthedocs conf.py is already in the doc folder
+  doc_path = '.'
+else:
+  # In all other builds conf.py is configured with cmake and put into the
+  # build folder.
+  doc_path = '@doc_path@'
 
 # -- General configuration ---------------------------------------------------
 
@@ -42,7 +53,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['@doc_path@/_templates']
+templates_path = [doc_path + '/_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -71,9 +82,6 @@ pygments_style = None
 
 # -- Options for HTML output -------------------------------------------------
 
-# is whether we are on readthedocs.io
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
@@ -91,7 +99,7 @@ else:
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['@doc_path@/_static']
+html_static_path = [doc_path + '/_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
