0ec8600f6ddfae3571482ded2551fd88bd1cb40d
Ddone <mynameisthenekit@gmail.com>
2019-01-11 23:09:22 +0200

    Now logging border sizes

--- src/components/bar.cpp
+++ src/components/bar.cpp
@@ -286,7 +286,10 @@ bar::bar(connection& conn, signal_emitter& emitter, const config& config, const 
   m_opts.center.x /= 2;
   m_opts.center.x += m_opts.borders[edge::LEFT].size;
 
-  m_log.info("Bar geometry: %ix%i+%i+%i", m_opts.size.w, m_opts.size.h, m_opts.pos.x, m_opts.pos.y);
+  m_log.info("Bar geometry: %ix%i+%i+%i; Borders: %d,%d,%d,%d", m_opts.size.w,
+      m_opts.size.h, m_opts.pos.x, m_opts.pos.y,
+      m_opts.borders[edge::TOP].size, m_opts.borders[edge::RIGHT].size,
+      m_opts.borders[edge::BOTTOM].size, m_opts.borders[edge::LEFT].size);
 
   m_log.trace("bar: Attach X event sink");
   m_connection.attach_sink(this, SINK_PRIORITY_BAR);
