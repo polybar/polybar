396f7a0ed520533556409ec74494af03aa9bbda1
Jérôme BOULMIER <jerome.boulmier@outlook.fr>
2019-06-10 21:37:25 -0400

    fix(x11): Don't include composite when disabled

--- include/x11/extensions/all.hpp
+++ include/x11/extensions/all.hpp
@@ -5,7 +5,9 @@
 #if WITH_XRANDR
 #include "x11/extensions/randr.hpp"
 #endif
+#if WITH_XCOMPOSITE
 #include "x11/extensions/composite.hpp"
+#endif
 #if WITH_XKB
 #include "x11/extensions/xkb.hpp"
 #endif
