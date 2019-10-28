ed8d0e95ea3059077377e49da150dfa9bf7717c2
patrick96 <p.ziegler96@gmail.com>
2019-10-28 01:35:18 +0100

    build: Move non-macro variables into settings.cpp
    Since APP_VERSION is different for every commit and almost all file
    include settings.hpp, the whole project has to be rebuilt for every
    commit. With this, hopefully, this can be greatly reduced and only
    changed files need to be rebuilt. This will also help ccache

--- include/settings.hpp.cmake
+++ include/settings.hpp.cmake
@@ -4,8 +4,8 @@
 #include <string>
 #include <vector>
 
-#define APP_NAME "@PROJECT_NAME@"
-#cmakedefine APP_VERSION "@APP_VERSION@"
+extern const char* const APP_NAME;
+extern const char* const APP_VERSION;
 
 #cmakedefine01 ENABLE_ALSA
 #cmakedefine01 ENABLE_MPD
@@ -59,54 +59,20 @@ static const int SINK_PRIORITY_SCREEN{2};
 static const int SINK_PRIORITY_TRAY{3};
 static const int SINK_PRIORITY_MODULE{4};
 
-static constexpr const char* ALSA_SOUNDCARD{"@SETTING_ALSA_SOUNDCARD@"};
-static constexpr const char* BSPWM_SOCKET_PATH{"@SETTING_BSPWM_SOCKET_PATH@"};
-static constexpr const char* BSPWM_STATUS_PREFIX{"@SETTING_BSPWM_STATUS_PREFIX@"};
-static constexpr const char* CONNECTION_TEST_IP{"@SETTING_CONNECTION_TEST_IP@"};
-static constexpr const char* PATH_ADAPTER{"@SETTING_PATH_ADAPTER@"};
-static constexpr const char* PATH_BACKLIGHT{"@SETTING_PATH_BACKLIGHT@"};
-static constexpr const char* PATH_BATTERY{"@SETTING_PATH_BATTERY@"};
-static constexpr const char* PATH_CPU_INFO{"@SETTING_PATH_CPU_INFO@"};
-static constexpr const char* PATH_MEMORY_INFO{"@SETTING_PATH_MEMORY_INFO@"};
-static constexpr const char* PATH_MESSAGING_FIFO{"@SETTING_PATH_MESSAGING_FIFO@"};
-static constexpr const char* PATH_TEMPERATURE_INFO{"@SETTING_PATH_TEMPERATURE_INFO@"};
+extern const char* const ALSA_SOUNDCARD;
+extern const char* const BSPWM_SOCKET_PATH;
+extern const char* const BSPWM_STATUS_PREFIX;
+extern const char* const CONNECTION_TEST_IP;
+extern const char* const PATH_ADAPTER;
+extern const char* const PATH_BACKLIGHT;
+extern const char* const PATH_BATTERY;
+extern const char* const PATH_CPU_INFO;
+extern const char* const PATH_MEMORY_INFO;
+extern const char* const PATH_MESSAGING_FIFO;
+extern const char* const PATH_TEMPERATURE_INFO;
 
-const auto version_details = [](const std::vector<std::string>& args) {
-  for (auto&& arg : args) {
-    if (arg.compare(0, 3, "-vv") == 0)
-      return true;
-  }
-  return false;
-};
+extern bool version_details(const std::vector<std::string>& args);
 
-// clang-format off
-const auto print_build_info = [](bool extended = false) {
-  printf("%s %s\n\n", APP_NAME, APP_VERSION);
-  printf("Features: %calsa %ccurl %ci3 %cmpd %cnetwork(%s) %cpulseaudio %cxkeyboard\n",
-    (ENABLE_ALSA       ? '+' : '-'),
-    (ENABLE_CURL       ? '+' : '-'),
-    (ENABLE_I3         ? '+' : '-'),
-    (ENABLE_MPD        ? '+' : '-'),
-    (ENABLE_NETWORK    ? '+' : '-'),
-    WIRELESS_LIB,
-    (ENABLE_PULSEAUDIO ? '+' : '-'),
-    (ENABLE_XKEYBOARD  ? '+' : '-'));
-  if (extended) {
-    printf("\n");
-    printf("X extensions: %crandr (%cmonitors) %ccomposite %cxkb %cxrm %cxcursor\n",
-      (WITH_XRANDR            ? '+' : '-'),
-      (WITH_XRANDR_MONITORS   ? '+' : '-'),
-      (WITH_XCOMPOSITE        ? '+' : '-'),
-      (WITH_XKB               ? '+' : '-'),
-      (WITH_XRM               ? '+' : '-'),
-      (WITH_XCURSOR           ? '+' : '-'));
-    printf("\n");
-    printf("Build type: @CMAKE_BUILD_TYPE@\n");
-    printf("Compiler: @CMAKE_CXX_COMPILER@\n");
-    printf("Compiler flags: @CMAKE_CXX_FLAGS@ ${CMAKE_CXX_FLAGS_${CMAKE_BUILD_TYPE_UPPER}}\n");
-    printf("Linker flags: @CMAKE_EXE_LINKER_FLAGS@ ${CMAKE_EXE_LINKER_FLAGS_${CMAKE_BUILD_TYPE_UPPER}}\n");
-  }
-};
-// clang-format on
+extern void print_build_info(bool extended = false);
 
 // vim:ft=cpp
