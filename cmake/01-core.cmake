923d9ae061b7d461c1caabdcb1347a049d18b86e
patrick96 <p.ziegler96@gmail.com>
2020-12-22 12:19:46 +0100

    Add cmake files for configuring individual targets
    This makes the code a bit less messy. We barely need if(BUILD_...)
    guards inside the cmake files, just the root CMakeLists.txt.

--- cmake/01-core.cmake
+++ cmake/01-core.cmake
@@ -1,12 +1,32 @@
 #
 # Core setup
 #
+option(DISABLE_ALL "Set this to ON disable all targets. Individual targets can be enabled explicitly." OFF)
 
-set(CMAKE_CXX_STANDARD 14)
-set(CMAKE_CXX_STANDARD_REQUIRED ON)
-set(CMAKE_CXX_EXTENSIONS OFF)
+# If all targets are disabled, we set the default value for options that are on
+# by default to OFF
+if (DISABLE_ALL)
+  set(DEFAULT_ON OFF)
+else()
+  set(DEFAULT_ON ON)
+endif()
 
-set(THREADS_PREFER_PTHREAD_FLAG ON)
+option(BUILD_POLYBAR "Build the main polybar executable" ${DEFAULT_ON})
+option(BUILD_POLYBAR_MSG "Build polybar-msg" ${DEFAULT_ON})
+option(BUILD_TESTS "Build testsuite" OFF)
+option(BUILD_DOC "Build documentation" ${DEFAULT_ON})
+
+if (BUILD_POLYBAR OR BUILD_TESTS)
+  set(BUILD_LIBPOLY ON)
+else()
+  set(BUILD_LIBPOLY OFF)
+endif()
+
+if (BUILD_LIBPOLY OR BUILD_POLYBAR_MSG)
+  set(HAS_CXX_COMPILATION ON)
+else()
+  set(HAS_CXX_COMPILATION OFF)
+endif()
 
 # Export compile commands used for custom targets
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
@@ -17,77 +37,3 @@ if(NOT CMAKE_BUILD_TYPE)
   message_colored(STATUS "No build type specified; using ${CMAKE_BUILD_TYPE}" 33)
 endif()
 string(TOUPPER ${CMAKE_BUILD_TYPE} CMAKE_BUILD_TYPE_UPPER)
-
-# Compiler flags
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wextra")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wpedantic")
-
-if (CMAKE_SYSTEM_NAME STREQUAL "FreeBSD")
-  # Need dprintf() for FreeBSD 11.1 and older
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D_WITH_DPRINTF")
-  # libinotify uses c99 extension, so suppress this error
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-c99-extensions")
-  # Ensures that libraries from dependencies in LOCALBASE are used
-  set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -L/usr/local/lib")
-endif()
-
-if(${CMAKE_CXX_COMPILER_ID} STREQUAL Clang)
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-error=parentheses-equality")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-zero-length-array")
-endif()
-
-set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -DDEBUG")
-set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -g2")
-
-if(${CMAKE_CXX_COMPILER_ID} STREQUAL GNU)
-  set(CMAKE_CXX_FLAGS_MINSIZEREL "${CMAKE_CXX_FLAGS_MINSIZEREL} -fdata-sections -ffunction-sections")
-  set(CMAKE_EXE_LINKER_FLAGS_MINSIZEREL "${CMAKE_EXE_LINKER_FLAGS_MINSIZEREL} -Wl,--gc-sections,--icf=safe")
-endif()
-
-# Check compiler
-if(${CMAKE_CXX_COMPILER_ID} STREQUAL Clang)
-  if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "3.4.0")
-    message_colored(FATAL_ERROR "Compiler not supported (Requires clang-3.4+ or gcc-5.1+)" 31)
-  else()
-    message_colored(STATUS "Using supported compiler ${CMAKE_CXX_COMPILER_ID}-${CMAKE_CXX_COMPILER_VERSION}" 32)
-  endif()
-elseif(${CMAKE_CXX_COMPILER_ID} STREQUAL GNU)
-  if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "5.1.0")
-    message_colored(FATAL_ERROR "Compiler not supported (Requires clang-3.4+ or gcc-5.1+)" 31)
-  else()
-    message_colored(STATUS "Using supported compiler ${CMAKE_CXX_COMPILER_ID}-${CMAKE_CXX_COMPILER_VERSION}" 32)
-  endif()
-else()
-  message_colored(WARNING "Using unsupported compiler ${CMAKE_CXX_COMPILER_ID}-${CMAKE_CXX_COMPILER_VERSION} !" 31)
-endif()
-
-# Set compiler and linker flags for preferred C++ library
-if(CXXLIB_CLANG)
-  message_colored(STATUS "Linking against libc++" 32)
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
-  set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -lc++ -lc++abi")
-elseif(CXXLIB_GCC)
-  message_colored(STATUS "Linking against libstdc++" 32)
-  set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -lstdc++")
-endif()
-
-# Custom build type ; SANITIZE
-SET(CMAKE_CXX_FLAGS_SANITIZE "-O0 -g -fsanitize=address -fsanitize=undefined -fno-omit-frame-pointer -fno-optimize-sibling-calls"
-  CACHE STRING "Flags used by the C++ compiler during sanitize builds." FORCE)
-SET(CMAKE_EXE_LINKER_FLAGS_SANITIZE ""
-  CACHE STRING "Flags used for linking binaries during sanitize builds." FORCE)
-SET(CMAKE_SHARED_LINKER_FLAGS_SANITIZE ""
-  CACHE STRING "Flags used by the shared libraries linker during sanitize builds." FORCE)
-MARK_AS_ADVANCED(
-  CMAKE_CXX_FLAGS_SANITIZE
-  CMAKE_EXE_LINKER_FLAGS_SANITIZE
-  CMAKE_SHARED_LINKER_FLAGS_SANITIZE)
-
-# Custom build type ; Coverage
-SET(CMAKE_CXX_FLAGS_COVERAGE
-  "${CMAKE_CXX_FLAGS_DEBUG} ${CMAKE_CXX_FLAGS_COVERAGE} --coverage")
-SET(CMAKE_EXE_LINKER_FLAGS_COVERAGE
-  "${CMAKE_EXE_LINKER_FLAGS_DEBUG} ${CMAKE_EXE_LINKER_FLAGS_COVERAGE}")
-SET(CMAKE_SHARED_LINKER_FLAGS_COVERAGE
-  "${CMAKE_SHARED_LINKER_FLAGS_DEBUG} ${CMAKE_SHARED_LINKER_FLAGS_COVERAGE}")
