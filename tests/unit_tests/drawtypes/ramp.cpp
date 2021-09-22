9c3551fbc95a47ee64fb40836f6e9ab41357952c
TheDoctor314 <darsh.rajan95@gmail.com>
2021-09-22 20:34:33 +0530

    Fix compile error in ramp test
    Use std::make_shared.

--- tests/unit_tests/drawtypes/ramp.cpp
+++ tests/unit_tests/drawtypes/ramp.cpp
@@ -27,9 +27,9 @@ TEST(Ramp, perc) {
 
 TEST(Ramp, weights) {
   ramp r;
-  r.add(factory_util::shared<label>("test1", 0), 1);
-  r.add(factory_util::shared<label>("test2", 0), 2);
-  r.add(factory_util::shared<label>("test3", 0), 5);
+  r.add(std::make_shared<label>("test1", 0), 1);
+  r.add(std::make_shared<label>("test2", 0), 2);
+  r.add(std::make_shared<label>("test3", 0), 5);
 
   EXPECT_EQ("test1", r.get_by_percentage(12)->get());
   EXPECT_EQ("test2", r.get_by_percentage(13)->get());
@@ -42,8 +42,8 @@ TEST(Ramp, weights) {
   EXPECT_EQ("test3", r.get_by_percentage_with_borders(41, 20, 40)->get());
   EXPECT_EQ("test1", r.get_by_percentage_with_borders(20, 20, 40)->get());
   EXPECT_EQ("test3", r.get_by_percentage_with_borders(40, 20, 40)->get());
-  r.add(factory_util::shared<label>("test4", 0));
-  r.add(factory_util::shared<label>("test5", 0));
+  r.add(std::make_shared<label>("test4", 0));
+  r.add(std::make_shared<label>("test5", 0));
   EXPECT_EQ("test2", r.get_by_percentage_with_borders(24, 20, 40)->get());
   EXPECT_EQ("test3", r.get_by_percentage_with_borders(25, 20, 40)->get());
 }
